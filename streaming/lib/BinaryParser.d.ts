7fe8eb34daa762158a232e4862434ec3dab19565
parik-shit <parikshitnasa@yahoo.in>
2024-10-04 07:41:42 +0530

    add api doc

---
+++ streaming/lib/BinaryParser.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="node" />
+export default class BinaryParser {
+    decompressZLib: (zlibData: Buffer) => Promise<Buffer>;
+    decodePKT: (pktBuff: Buffer) => number | {
+        pktLen: number;
+        jData: any;
+    };
+    private frameFromSpec;
+    private formatFunc;
+    private decodeL1PKT;
+    decodeL2PKT: (specMatrix: any, pktLen: number, pktBuff: Buffer) => any;
+    private decodeOhlcPKT;
+    private decodeMarketStatus;
+    private decodeMessage;
+    private decodeStatus;
+    private decodeGreeks;
+}
